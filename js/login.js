9feeef4a499bdaac65df15aaaa3dfd2ba8c47773
Young <abiodunadebambo44@gmail.com>
2020-06-28 02:19:38 +0100

    added preloader to the login page and style the registration page

--- js/login.js
+++ js/login.js
@@ -1,14 +1,25 @@
 const form = document.getElementById("login-form");
 
-document.getElementById('login-btn').addEventListener("click",async (e) => {
+document.getElementById('login-btn').addEventListener("click", async (e) => {
     e.preventDefault();
 
     //get the username and password input
     const usernameUI = document.getElementById("email-username").value;
     const passwordUI = document.getElementById("password").value
     
+    // remove any message element
+    removeMessage();
+    
     // if any field is empty stop
-    if (!usernameUI || !passwordUI) return;
+    if (!usernameUI || !passwordUI) {
+        // show message saying you should enter a value
+        showMessage("error", "Please, enter your username and password.");
+        
+        return
+    };
+
+    // show preloader
+    document.querySelector(".preloader").style.display = "inline";
 
     // get the list of registered users
     const users = await getUsers();
@@ -22,21 +33,26 @@ document.getElementById('login-btn').addEventListener("click",async (e) => {
         }
     })
 
-    if(isValid){
-        // login
-        // !form.dispatchEvent(evt);
-        form.submit();
-        console.log("Successful login");
-        return
-    }else {
-        // show error message
-        console.log("no match found");
-    }
+    // remove the preloader and act after 2 seconds
+    setTimeout(() => {
+        // remove preloader
+        document.querySelector(".preloader").style.display = "none";
+
+        if(isValid){
+            // login
+            // !form.dispatchEvent(evt);
+            form.submit();
+            console.log("Successful login");
+            return
+        }else {
+            // show error message
+            showMessage("error", "Invalid username/email or password.");
+
+            console.log("no match found");
+        }
+    },2000)
     // console.log(users, usernameUI, passwordUI);
 });
-
-
-
 
 // get users   
 const getUsers = () => {
@@ -58,4 +74,32 @@ const getUsers = () => {
         })
         // catch any error and console it
         .catch(err => console.log(err));
+}
+
+// show message
+const showMessage = (status, messageText) => {
+    // create an element for the error
+    const message = document.createElement("p");
+
+    // add class od status to it
+    message.className = `message-alert ${status}`;
+
+    // add text content
+    message.textContent = messageText;
+
+    // get the parent and append it to it
+    const parent = document.querySelector(".message");
+
+    parent.appendChild(message);
+
+    // remove it after 3 secs
+    setTimeout(removeMessage, 3000)
+}
+
+const removeMessage = () => {
+    // get any tag with class of message alert and remove it
+    const message = document.querySelector(".message-alert");
+    if (message){
+        message.remove();
+    }
 }
